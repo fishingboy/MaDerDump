5f2c550050dced996fea864ec90b8b51194c6390
Leo Kuo <fishingboy@gmail.com>
2021-10-01 17:51:58 +0800

    基本的值

--- src/MaDump.php
+++ src/MaDump.php
@@ -7,9 +7,25 @@ namespace Fishingboy\MaDump;
  */
 class MaDump
 {
-    public function dump()
+    public function dump($data, $deep = 0)
     {
+        if (is_object($data)) {
+            echo get_class($data) . " ==>\n";
+            foreach ($data as $key => $value) {
+                echo $this->getPadding($deep) . "[+]$key\n";
+            }
+        } else if (is_array($data)) {
+            echo "Array => \n";
+            foreach ($data as $key => $value) {
+                echo $this->getPadding($deep) . "[+]$key\n";
+            }
+        }
         return true;
+    }
+
+    public function getPadding($deep)
+    {
+        return str_pad(" ", ($deep + 1) * 4);
     }
 }
 
--- tests/DumpTest.php
+++ tests/DumpTest.php
@@ -1,15 +1,34 @@
 <?php
 
-//use ;
 use Fishingboy\MaDump\MaDump;
 use PHPUnit\Framework\TestCase;
 
 class DumpTest extends TestCase
 {
-    public function test_dump()
+    public function test_dump_array()
     {
         $dump = new MaDump();
-        $response = $dump->dump();
+        $response = $dump->dump([1]);
         $this->assertTrue($response);
     }
+
+    public function test_dump_object()
+    {
+        $dump = new MaDump();
+        $object = (object) ["a" => 1];
+        $response = $dump->dump($object);
+        $this->assertTrue($response);
+    }
+
+    public function test_dump_object2()
+    {
+        $dump = new MaDump();
+        $object = new Car();
+        $response = $dump->dump($object);
+        $this->assertTrue($response);
+    }
+}
+
+class Car {
+    public $length = 1;
 }
