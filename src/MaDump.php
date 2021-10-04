9ff82a081d49a465e720f177a3004c759b563741
Leo Kuo <fishingboy@gmail.com>
2021-10-04 17:44:03 +0800

    完成主要的結構

--- src/MaDump.php
+++ src/MaDump.php
@@ -9,33 +9,34 @@ class MaDump
 {
     public function dump($data, $deep = 0)
     {
+        echo "<pre>";
         if (is_object($data)) {
-            echo get_class($data) . "\n";
+            echo get_class($data) . "<br>\n";
             foreach ($data as $key => $value) {
                 if (is_object($value)) {
                     $class = get_class($value);
-                    echo $this->getPadding($deep) . ".$key + ($class) \n";
+                    echo $this->getPadding($deep) . ".$key + ($class) <br>\n";
                 } else if (is_array($value)) {
                     if ($this->isNormalArray($value)) {
-                        echo $this->getPadding($deep) . ".$key (Array) \n";
+                        echo $this->getPadding($deep) . ".$key (Array) <br>\n";
                     } else {
-                        echo $this->getPadding($deep) . ".$key + (Array) \n";
+                        echo $this->getPadding($deep) . ".$key + (Array) <br>\n";
                     }
                 } else {
-                    echo $this->getPadding($deep) . ".$key = {$value} \n";
+                    echo $this->getPadding($deep) . ".$key = {$value} <br>\n";
                 }
             }
 
             $methods = get_class_methods($data);
             if (count($methods)) {
                 foreach ($methods as $method) {
-                    echo $this->getPadding($deep) . "->$method()\n";
+                    echo $this->getPadding($deep) . "->$method()<br>\n";
                 }
             }
         } else if (is_array($data)) {
-            echo "Array => \n";
+            echo "Array => <br>\n";
             foreach ($data as $key => $value) {
-                echo $this->getPadding($deep) . ".$key\n";
+                echo $this->getPadding($deep) . ".$key<br>\n";
             }
         }
         return true;
