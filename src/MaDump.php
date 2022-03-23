3cfb6ef5cabb5ee8a2d6b16af909af687311ba8d
Leo Kuo <fishingboy@gmail.com>
2022-03-23 12:42:00 +0800

    加上 getter method 的回傳值

--- src/MaDump.php
+++ src/MaDump.php
@@ -7,6 +7,8 @@ namespace Fishingboy\MaDump;
  */
 class MaDump
 {
+    const VAR_TYPE_BOOLEAN = "boolean";
+
     /**
      * dump data
      * @param $data
@@ -23,22 +25,37 @@ class MaDump
             foreach ($data as $key => $value) {
                 if (is_object($value)) {
                     $class = get_class($value);
-                    $attributes[] = self::getPadding($deep) . ".$key + ($class)";
+                    $attributes[] = self::getPadding($deep) . "[$key] ($class)";
                 } else if (is_array($value)) {
                     if (self::isNormalArray($value)) {
-                        $attributes[] = self::getPadding($deep) . ".$key (Array)";
+                        $attributes[] = self::getPadding($deep) . "[$key] (Array)";
                     } else {
-                        $attributes[] = self::getPadding($deep) . ".$key + (Array)";
+                        $attributes[] = self::getPadding($deep) . "[$key] (Key Value Array)";
                     }
                 } else {
-                    $attributes[] = self::getPadding($deep) . ".$key = {$value}";
+                    $attributes[] = self::getPadding($deep) . "[$key] => " . self::dumpValue($value);
                 }
             }
 
             $methods = get_class_methods($data);
             if (count($methods)) {
                 foreach ($methods as $method) {
-                    $attributes[] = self::getPadding($deep) . "->$method()";
+                    $attribute_output = self::getPadding($deep) . "->$method()";
+                    if (preg_match("/^[a-z]+[A-Z]+/", $method)) {
+                        $method_return = $data->$method();
+                        if (is_object($method_return)) {
+                            $attribute_output .= " : " . get_class($method_return);
+                        } elseif (is_array($method_return)) {
+                            if (self::isNormalArray($method_return)) {
+                                $attribute_output .= " : array";
+                            } else {
+                                $attribute_output .= " : key value array";
+                            }
+                        } else {
+                            $attribute_output .= " : " . self::dumpValue($method_return);
+                        }
+                    }
+                    $attributes[] = $attribute_output;
                 }
             }
         } else if (is_array($data)) {
@@ -51,13 +68,12 @@ class MaDump
                 } else if (is_array($value)) {
                     $value_output = "=> (Array)";
                 } else {
-                    $value_output = "=> $value";
+                    $value_output = "=> " . self::dumpValue($value);
                 }
                 $attributes[] = self::getPadding($deep) . "[{$key}] $value_output";
             }
         } else {
-            $output .= $data;
-            $output .= " (" . gettype($data) . ")";
+            $output .= self::dumpValue($data);
         }
 
         sort($attributes);
@@ -71,12 +87,23 @@ class MaDump
         }
     }
 
-    public static function getPadding($deep): string
+    private static function dumpValue($value)
+    {
+        $type = gettype($value);
+        $value_output = $value;
+        if ($type == self::VAR_TYPE_BOOLEAN) {
+            $value_output = ($value) ? "true" : "false";
+        }
+
+        return "$value_output ($type)";
+    }
+
+    private static function getPadding($deep): string
     {
         return str_pad(" ", ($deep + 1) * 4);
     }
 
-    public static function isNormalArray($array): bool
+    private static function isNormalArray($array): bool
     {
         foreach ($array as $key => $value) {
             if ($key != (string) intval($key)) {
--- tests/DumpTest.php
+++ tests/DumpTest.php
@@ -5,45 +5,51 @@ use PHPUnit\Framework\TestCase;
 
 class DumpTest extends TestCase
 {
+    public function test_dump_value_int()
+    {
+        $data = 1;
+        $response = MaDump::dump($data, true);
+        $this->assertEquals("<pre>1 (integer)</pre>", $response);
+    }
+
+    public function test_dump_value_bool()
+    {
+        $data = true;
+        $dump = new MaDump();
+        $response = MaDump::dump($data, true);;
+        $this->assertEquals("<pre>true (boolean)</pre>", $response);
+    }
+
     public function test_dump_array()
     {
-        $dump = new MaDump();
-        $response = $dump->dump([1]);
-        $this->assertTrue($response);
+        $data = [1];
+        var_dump($data);
+        $response = MaDump::dump($data, true);
+        $this->assertEquals("<pre>Array(1) => \n    [0] => 1 (integer)</pre>", $response);
     }
 
     public function test_dump_object()
     {
-        $dump = new MaDump();
         $object = (object) ["a" => 1];
-        $response = $dump->dump($object);
-        $this->assertTrue($response);
+        $response = MaDump::dump($object, true);
+        echo $response;
+        $this->assertEquals("<pre>stdClass
+    [a] => 1 (integer)</pre>", $response);
     }
 
     public function test_dump_object2()
     {
-        $dump = new MaDump();
         $object = new Car();
-        $response = $dump->dump($object);
-        $this->assertTrue($response);
-    }
-
-    public function test_is_normal_array()
-    {
-        $dump = new MaDump();
-        $response = $dump->isNormalArray([1,2,3]);
-        $this->assertTrue($response);
-    }
-
-    public function test_is_normal_array2()
-    {
-        $dump = new MaDump();
-        $response = $dump->isNormalArray([
-            "a" => 1,
-            "b" => 1,
-            "c" => 1,
-        ]);
-        $this->assertFalse($response);
+        $response = MaDump::dump($object, true);
+        echo $response;
+        $this->assertEquals("<pre>Car
+    ->__construct()
+    ->getLength() : 1 (integer)
+    ->getLight() : Light
+    [attrs] (Key Value Array)
+    [items] (Array)
+    [length] => 1 (integer)
+    [light] (Light)</pre>", $response);
     }
 }
 
@@ -67,6 +73,11 @@ class Car {
     public function getLength(): int
     {
         return $this->length;
+    }
+
+    public function getLight(): Light
+    {
+        return $this->light;
     }
 }
 
