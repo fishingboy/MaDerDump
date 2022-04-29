0ab7811863a8211216a3d1cef8335cdf7447be0d
Leo Kuo <fishingboy@gmail.com>
2022-04-29 18:27:39 +0800

    add MaDump::who_call_me()

--- src/MaDump.php
+++ src/MaDump.php
@@ -75,4 +75,27 @@ class MaDump
         }
         return implode(", ", $output);
     }
+
+    /**
+     * 取得誰呼叫這個 method
+     * @param integer $level 顯示的層數
+     * @return string         呼叫的結構
+     */
+    public static function who_call_me($level = 999)
+    {
+        $trace_info = debug_backtrace();
+        $infos = [];
+        for ($i = 1, $i_max = count($trace_info); $i < 1 + $level && $i < $i_max; $i++) {
+            $info = $trace_info[$i];
+            $msg = "";
+            $msg .= (isset($info['file'])) ? "file:{$info['file']} - " : "";
+            $msg .= (isset($info['line'])) ? "line:{$info['line']} : " : "";
+            $msg .= (isset($info['class'])) ? "{$info['class']}::" : "";
+            $msg .= "{$info['function']}()";
+            $infos[] = $msg;
+        }
+        $msg = implode("
+\r\n --> ", array_reverse($infos));
+        return $msg;
+    }
 }
